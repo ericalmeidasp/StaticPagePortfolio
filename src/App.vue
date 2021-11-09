45587067b424838d860046c12f490c9bc143da21
Éric Almeida <ericalmeida.sp@gmail.com>
2021-11-08 23:08:55 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="component">
+    <HomeTemplate />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { HomeTemplate } from './components/templates'
+
+export default defineComponent({ components: { HomeTemplate } })
+</script>
+
---
+++ src/components/organisms/Services.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front End"
+      description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, inventore! Ex maxime veniam necessitatibus vitae ab voluptates quibusdam ullam repellendus, non et quae reprehenderit sint nam id dolores ut facere."
+    />
+    <Service
+      title="Back End"
+      description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, inventore! Ex maxime veniam necessitatibus vitae ab voluptates quibusdam ullam repellendus, non et quae reprehenderit sint nam id dolores ut facere."
+    />
+    <Service
+      title="Manutenção"
+      description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, inventore! Ex maxime veniam necessitatibus vitae ab voluptates quibusdam ullam repellendus, non et quae reprehenderit sint nam id dolores ut facere."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Service } from '@/components/molecules'
+
+export default defineComponent({ components: { Service } })
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
